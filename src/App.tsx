aeeb6b489f66b49c4402ef6f4145413d3bad453c
louish <louis-arnaud.heufke@epfl.ch>
2024-05-22 15:27:01 +0200

    [add] epfl element stylesheet and react components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css'
+import 'epfl-elements/dist/css/elements.css'
+import 'epfl-elements/dist/css/vendors.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
 import NotFound from './Pages/NotFound'
 
 function App() {
-  console.log('0.0.1')
+  console.log('0.0.2')
   return (
     <Router basename={import.meta.env.DEV ? '/' : '/EPFL-News-Reader/'}>
       <Routes>
