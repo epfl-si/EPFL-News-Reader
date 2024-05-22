605a9b503791dbbc38291974a7355397e8566344
louish <louis-arnaud.heufke@epfl.ch>
2024-05-22 10:40:28 +0200

    [feature] GitHub action deploy to page

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import Home from './Pages/Home'
 import NotFound from './Pages/NotFound'
 
 function App() {
+  console.log('0.0.1')
   return (
-    <Router>
+    <Router basename={import.meta.env.DEV ? '/' : '/EPFL-News-Reader/'}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
+
   )
 }
 
